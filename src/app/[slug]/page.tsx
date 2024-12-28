3babae23c7ee1ba58997acb24f7419e38e5dd0e8
Emmanuel Sunday <ebubesunday16@gmail.com>
2024-12-28 10:25:38 +0100

    Fixing Type Issues With Promise In Dynamic Router

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -8,7 +8,7 @@ interface PageProps {
     params: Promise<{
         slug: string;
     }>;
-    searchParams?: { [key: string]: string | string[] | undefined };
+    searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }
 
 export async function generateMetadata(
